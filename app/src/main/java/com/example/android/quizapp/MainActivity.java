7b8efd91dd27e9022a3a6d2e735c44549ac71ddb
harshabhadra <harshasharkey@gmail.com>
2018-08-05 22:19:52 +0530

    Add app and gradle file

---
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitAnswer(View view){
+
+    }
+}
